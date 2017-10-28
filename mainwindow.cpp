dde9cf7fd43993ec7a6963b501acaadeb38ed448
EdXian <f29993856@gmail.com>
2017-10-28 15:45:52 +0800

    runjs error

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,28 +6,24 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //QUrl url1("https://github.com/");       // strMapPath为QString类，是你html文件的路径
-    QUrl url("qrc:/map.html");
 
-    QWebEngineSettings::globalSettings()->setAttribute(QWebEngineSettings::PluginsEnabled,true);
-    QWebEngineSettings::globalSettings()->setAttribute(QWebEngineSettings::JavascriptEnabled,true);
-    QWebEnginePage *page = new QWebEnginePage(this);  // 定义一个page作为页面管理
-    QWebChannel *channel = new QWebChannel(this);     // 定义一个channel作为和JS或HTML交互
-    page->load(url);                         // page上加载html路径
-    page->setWebChannel(channel);                   // 把channel配置到page上，让channel作为其信使
-    ui->widget->setPage(page);                   // 建立page和UI上的webEngine的联系
+       QUrl url("qrc:/map.html");
 
+       QWebEngineSettings::globalSettings()->setAttribute(QWebEngineSettings::PluginsEnabled,true);
+       QWebEngineSettings::globalSettings()->setAttribute(QWebEngineSettings::JavascriptEnabled,true);
+       QWebEnginePage *page = new QWebEnginePage(this);
+       QWebChannel *channel = new QWebChannel(this);
+       page->load(url);
+       page->setWebChannel(channel);
+       ui->widget->setPage(page);
 
-    //QString cmd("document.documentElement.contentEditable = true");
         QString js=QString("var marker = new google.maps.Marker({ ")+
         QString("position: new google.maps.LatLng(24.705311, -74.2581939),")+
         QString(" map: map,")+
         QString("title: \"Marker A\",")+
         QString("   }); ");
 
-    //qDebug()<<js;
-    //ui->widget->page()->runJavaScript(cmd,[](const QVariant &result){ qDebug() << result.toBool(); });
-    ui->widget->page()->runJavaScript(js,[](const QVariant &result){ qDebug() << result.toBool(); });
+       ui->widget->page()->runJavaScript(js,[](const QVariant &result){ qDebug() << result.toBool(); });
 
 
 }
